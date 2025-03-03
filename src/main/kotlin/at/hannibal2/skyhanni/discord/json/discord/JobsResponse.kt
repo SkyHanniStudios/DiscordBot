1b610996c0f9c7d082123e295039adffe008ecc1
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-03-03 21:12:25 +0100

    adding @SerializedName everywhere

--- src/main/kotlin/at/hannibal2/skyhanni/discord/json/discord/JobsResponse.kt
+++ src/main/kotlin/at/hannibal2/skyhanni/discord/json/discord/JobsResponse.kt
@@ -1,41 +1,43 @@
 package at.hannibal2.skyhanni.discord.json.discord
 
+import com.google.gson.annotations.SerializedName
+
 data class JobsResponse(
-    val total_count: Int,
-    val jobs: List<Job>
+    @SerializedName("total_count") val totalCount: Int,
+    @SerializedName("jobs") val jobs: List<Job>
 )
 
 data class Job(
-    val id: Long,
-    val run_id: Long,
-    val workflow_name: String,
-    val head_branch: String,
-    val run_url: String,
-    val run_attempt: Int,
-    val node_id: String,
-    val head_sha: String,
-    val url: String,
-    val html_url: String,
-    val status: String,
-    val conclusion: String,
-    val created_at: String,
-    val started_at: String,
-    val completed_at: String,
-    val name: String,
-    val steps: List<Step>,
-    val check_run_url: String,
-    val labels: List<String>,
-    val runner_id: Long?,
-    val runner_name: String?,
-    val runner_group_id: Long?,
-    val runner_group_name: String?
+    @SerializedName("id") val id: Long,
+    @SerializedName("run_id") val runId: Long,
+    @SerializedName("workflow_name") val workflowName: String,
+    @SerializedName("head_branch") val headBranch: String,
+    @SerializedName("run_url") val runUrl: String,
+    @SerializedName("run_attempt") val runAttempt: Int,
+    @SerializedName("node_id") val nodeId: String,
+    @SerializedName("head_sha") val headSha: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("html_url") val htmlUrl: String,
+    @SerializedName("status") val status: String,
+    @SerializedName("conclusion") val conclusion: String,
+    @SerializedName("created_at") val createdAt: String,
+    @SerializedName("started_at") val startedAt: String,
+    @SerializedName("completed_at") val completedAt: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("steps") val steps: List<Step>,
+    @SerializedName("check_run_url") val checkRunUrl: String,
+    @SerializedName("labels") val labels: List<String>,
+    @SerializedName("runner_id") val runnerId: Long?,
+    @SerializedName("runner_name") val runnerName: String?,
+    @SerializedName("runner_group_id") val runnerGroupId: Long?,
+    @SerializedName("runner_group_name") val runnerGroupName: String?
 )
 
 data class Step(
-    val name: String,
-    val status: String,
-    val conclusion: String,
-    val number: Int,
-    val started_at: String,
-    val completed_at: String
+    @SerializedName("name") val name: String,
+    @SerializedName("status") val status: String,
+    @SerializedName("conclusion") val conclusion: String,
+    @SerializedName("number") val number: Int,
+    @SerializedName("started_at") val startedAt: String,
+    @SerializedName("completed_at") val completedAt: String
 )
