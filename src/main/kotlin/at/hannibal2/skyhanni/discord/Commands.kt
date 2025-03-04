726ed3ce251e8a8311ada9e0a263d6b52235f99b
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2025-03-04 02:03:12 +0100

    add data + specific help command

--- src/main/kotlin/at/hannibal2/skyhanni/discord/Commands.kt
+++ src/main/kotlin/at/hannibal2/skyhanni/discord/Commands.kt
@@ -4,7 +4,11 @@ import at.hannibal2.skyhanni.discord.Utils.messageDelete
 import at.hannibal2.skyhanni.discord.Utils.reply
 import at.hannibal2.skyhanni.discord.Utils.replyWithConsumer
 import at.hannibal2.skyhanni.discord.Utils.runDelayed
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.awt.Color
+import java.time.Instant
 import kotlin.time.Duration.Companion.seconds
 
 @Suppress("UNUSED_PARAMETER")
@@ -29,16 +33,16 @@ class Commands(private val config: BotConfig) {
     fun onMessage(bot: DiscordBot, event: MessageReceivedEvent) {
         if (event.guild.id != bot.config.allowedServerId) return
 
-        val autor = event.author
-        if (autor.isBot) {
-            if (autor.id == botId) {
+        val author = event.author
+        if (author.isBot) {
+            if (author.id == botId) {
                 BotMessageHandler.handle(event)
             }
             return
         }
         val content = event.message.contentRaw.trim()
         if (content != "!undo") {
-            tagCommands.lastMessages.remove(autor.id)
+            tagCommands.lastMessages.remove(author.id)
         }
 
         if (!isCommand(content)) return
@@ -77,20 +81,32 @@ class Commands(private val config: BotConfig) {
 
     private fun MessageReceivedEvent.helpCommand(args: List<String>) {
         val hasAdminPerms = hasAdminPermissions()
-        val commands = if (hasAdminPerms && inBotCommandChannel()) {
-            commands
-        } else {
-            commands.filter { it.userCommand }
-        }
-        val list = commands.joinToString(", !", prefix = "!") { it.name }
-        reply("Supported commands: $list")
 
-        if (hasAdminPermissions() && !inBotCommandChannel()) {
-            val id = config.botCommandChannelId
-            val botCommandChannel = "https://discord.com/channels/$id/$id"
-            replyWithConsumer("You wanna see the cool admin only commands? visit $botCommandChannel") { consumer ->
-                runDelayed(3.seconds) {
-                    consumer.message.messageDelete()
+        if (args.size >= 2) {
+            val commandName = args[1].lowercase()
+            val command = CommandsData.getCommand(commandName) ?: return
+
+            if (!command.userCommand && !hasAdminPerms) return
+
+            val embed = this.createHelpEmbed(commandName, command)
+
+            this.reply(embed)
+        } else {
+            val commands = if (hasAdminPerms && inBotCommandChannel()) {
+                commands
+            } else {
+                commands.filter { it.userCommand }
+            }
+            val list = commands.joinToString(", !", prefix = "!") { it.name }
+            reply("Supported commands: $list")
+
+            if (hasAdminPerms && !inBotCommandChannel()) {
+                val id = config.botCommandChannelId
+                val botCommandChannel = "https://discord.com/channels/$id/$id"
+                replyWithConsumer("You wanna see the cool admin only commands? visit $botCommandChannel") { consumer ->
+                    runDelayed(3.seconds) {
+                        consumer.message.messageDelete()
+                    }
                 }
             }
         }
@@ -103,6 +119,26 @@ class Commands(private val config: BotConfig) {
     }
 
     fun existCommand(text: String): Boolean = commands.find { it.name.equals(text, ignoreCase = true) } != null
+
+    private fun MessageReceivedEvent.createHelpEmbed(commandName: String, command: CommandData): MessageEmbed {
+        val name = commandName.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        val description = command.description
+        val options = command.options.joinToString("\n")
+
+        val em = EmbedBuilder()
+
+        em.setTitle(description)
+        em.setColor(Color.GREEN)
+
+        for (option in command.options) {
+            em.addField(option.name, option.description, true)
+            em.addField("Required", if (option.required) "✅" else "❌", true)
+            em.addBlankField(true)
+        }
+
+        return em.build()
+//        val category = command.category 📁 Category: **$category**
+    }
 }
 
 class Command(
