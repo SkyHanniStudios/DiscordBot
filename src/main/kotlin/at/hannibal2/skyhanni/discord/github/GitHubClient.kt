e3ad6c60813431e104fadc08f1142ae6c952e425
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-03-05 12:59:04 +0100

    code cleanup in GitHubClient

--- src/main/kotlin/at/hannibal2/skyhanni/discord/github/GitHubClient.kt
+++ src/main/kotlin/at/hannibal2/skyhanni/discord/github/GitHubClient.kt
@@ -10,54 +10,45 @@ import at.hannibal2.skyhanni.discord.json.discord.PullRequestJson
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import java.io.File
 
-class GitHubClient(private val owner: String, private val repo: String, private val token: String) {
+class GitHubClient(owner: String, repo: String, private val token: String) {
     private val client = OkHttpClient()
     private val gson = Gson()
+    private val base = "https://api.github.com/repos/$owner/$repo"
 
-    fun findArtifact(commitSha: String): Artifact? {
-        val url = "https://api.github.com/repos/$owner/$repo/actions/artifacts"
-        val artifactsRequest = Request.Builder().url(url).header("Authorization", "token $token").build()
-
-        client.newCall(artifactsRequest).execute().use { artifactsResponse ->
-            if (!artifactsResponse.isSuccessful) error("Error fetching artifacts: ${artifactsResponse.code}")
-            val json = artifactsResponse.body?.string() ?: return null
+    fun findArtifact(lastCommit: String): Artifact? {
+        response("$base/actions/artifacts").use {
+            if (!it.isSuccessful) error("Error fetching artifacts: ${it.code}")
+            val json = it.body?.string() ?: return null
             val response = gson.fromJson(json, ArtifactResponse::class.java)
-            return response.artifacts.firstOrNull {
-                it.workflowRun?.headSha == commitSha && it.name == "Development Build"
+            return response.artifacts.firstOrNull { artifact ->
+                artifact.workflowRun?.headSha == lastCommit && artifact.name == "Development Build"
             }
         }
     }
 
     fun downloadArtifact(artifactId: Int, outputFile: File) {
-        val url = "https://api.github.com/repos/$owner/$repo/actions/artifacts/$artifactId/zip"
-        val request = Request.Builder().url(url).header("Authorization", "token $token").build()
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) error("Artifact download error: ${response.code} - ${response.message}")
-            outputFile.writeBytes(response.body?.bytes() ?: ByteArray(0))
+        response("$base/actions/artifacts/$artifactId/zip").use {
+            if (!it.isSuccessful) error("Artifact download error: ${it.code} - ${it.message}")
+            outputFile.writeBytes(it.body?.bytes() ?: ByteArray(0))
         }
     }
 
     fun findPullRequest(prNumber: Int): PullRequestJson? {
-        val request = Request.Builder().url("https://api.github.com/repos/$owner/$repo/pulls/$prNumber")
-            .header("Authorization", "token $token").build()
-
-        client.newCall(request).execute().use { response ->
+        response("$base/pulls/$prNumber").use {
             val gson = Gson()
-            if (!response.isSuccessful) error("GitHub API error: ${response.code}")
-            val json = response.body?.string() ?: return null
+            if (!it.isSuccessful) error("GitHub API error: ${it.code}")
+            val json = it.body?.string() ?: return null
             return gson.fromJson(json, PullRequestJson::class.java)
         }
     }
 
     fun getRun(commitSha: String, checkName: String): CheckRun? {
-        val url = "https://api.github.com/repos/$owner/$repo/commits/$commitSha/check-runs?check_name=$checkName"
-        val checkRunsRequest = Request.Builder().url(url).header("Authorization", "token $token").build()
-
-        client.newCall(checkRunsRequest).execute().use { checkRunsResponse ->
-            if (!checkRunsResponse.isSuccessful) error("Error fetching artifacts: ${checkRunsResponse.code}")
-            val json = checkRunsResponse.body?.string() ?: return null
+        response("$base/commits/$commitSha/check-runs?check_name=$checkName").use {
+            if (!it.isSuccessful) error("Error fetching artifacts: ${it.code}")
+            val json = it.body?.string() ?: return null
             val response = gson.fromJson(json, CheckRunsResponse::class.java)
             if (response.totalCount == 0) return null
             return response.checkRuns.firstOrNull()
@@ -65,15 +56,17 @@ class GitHubClient(private val owner: String, private val repo: String, private 
     }
 
     // might come handy later
-    fun getJob(owner: String, repo: String, artifactId: String, token: String): Job? {
-        val url = "https://api.github.com/repos/$owner/$repo/actions/runs/$artifactId/jobs"
-        val artifactsRequest = Request.Builder().url(url).header("Authorization", "token $token").build()
-
-        client.newCall(artifactsRequest).execute().use { artifactsResponse ->
-            if (!artifactsResponse.isSuccessful) error("Error fetching jobs: ${artifactsResponse.code}")
-            val json = artifactsResponse.body?.string() ?: return null
+    fun getJob(artifactId: String): Job? {
+        response("$base/actions/runs/$artifactId/jobs").use {
+            if (!it.isSuccessful) error("Error fetching jobs: ${it.code}")
+            val json = it.body?.string() ?: return null
             val response = gson.fromJson(json, JobsResponse::class.java)
-            return response.jobs.firstOrNull { it.name == "Build and test" }
+            return response.jobs.firstOrNull { job -> job.name == "Build and test" }
         }
+    }
+
+    private fun response(url: String): Response {
+        val request = Request.Builder().url(url).header("Authorization", "token $token").build()
+        return client.newCall(request).execute()
     }
 }
