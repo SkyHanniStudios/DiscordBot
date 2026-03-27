0f18cd102936c846ece9b297beeb5556f93ab8c2
nopo <nopotheemail@gmail.com>
2026-03-27 16:54:58 +1100

    make this less annoying clueless

--- src/main/kotlin/at/hannibal2/skyhanni/discord/command/PleadReactor.kt
+++ src/main/kotlin/at/hannibal2/skyhanni/discord/command/PleadReactor.kt
@@ -1,12 +1,24 @@
 package at.hannibal2.skyhanni.discord.command
 
+import at.hannibal2.skyhanni.discord.PLEADING_FACE
+import at.hannibal2.skyhanni.discord.Utils.reply
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.internal.entities.emoji.CustomEmojiImpl
+import kotlin.random.Random
 
 object PleadReactor {
 
+    val customPlead = CustomEmojiImpl("pleading_face", 1484796321901838467, false)
+
     fun doPleadReact(event: MessageReceivedEvent, message: String) {
         if (!message.contains(":pleading_face:") && !message.contains("\uD83E\uDD7A")) return
-        event.message.addReaction(CustomEmojiImpl("pleading_face", 1484796321901838467, false)).queue()
+        val random = Random.nextInt(100)
+        if (random == 0) {
+            event.reply(PLEADING_FACE)
+        } else if (random < 7) {
+            event.reply(customPlead.asMention)
+        } else if (random < 25) {
+            event.message.addReaction(customPlead).queue()
+        }
     }
 }
